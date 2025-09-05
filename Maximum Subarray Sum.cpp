4362af87f0b806a66a3483d6765fafaf123df3c7
F20240644 <ayushkumarsingh.github@gmail.com>
2025-09-05 10:08:47 +0530

    Solved 4 questions

---
+++ Maximum Subarray Sum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <climits>
+
+int main(){
+    int n;
+    std::cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++) std::cin>>arr[i];
+    long long currSum=0, maxSum=LLONG_MIN;
+    for(int x : arr){
+        currSum+=x;
+        maxSum=std::max(currSum,maxSum);
+        if(currSum<0) currSum=0;
+    }
+    std::cout<<maxSum<<"\n";
+    return 0;
+}
---
+++ Movie Festival.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <utility>
+#include <algorithm>
+
+int main(){
+    int n;
+    std::cin>>n;
+    std::vector<std::pair<int,int>>v(n);
+    for(int i=0;i<n;i++){
+        std::cin>>v[i].first>>v[i].second;
+    }
+    sort(v.begin(), v.end(), [](auto p1, auto p2){
+        if(p1.second!=p2.second) return p1.second<p2.second;
+        return p1.first<p2.first;
+    });
+    int ans=0;
+    std::pair<int,int>curr={0,0};
+    for(auto p : v){
+        if(curr==std::pair<int,int>(0,0)||curr.second<=p.first){
+            ans++;
+            curr=p;
+        }
+    }
+    std::cout<<ans<<"\n";
+    return 0;
+}
